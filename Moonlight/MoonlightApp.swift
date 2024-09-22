08ca239ac94cf81af433e841ce50bf5dd6c987dd
MJMayankMJ <226301131@gkv.ac.in>
2024-09-22 20:12:54 +0530

    Initial Commit

---
+++ Moonlight/MoonlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonlightApp.swift
+//  Moonlight
+//
+//  Created by Mayank Jangid on 9/22/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoonlightTests/MoonlightTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoonlightTests.swift
+//  MoonlightTests
+//
+//  Created by Mayank Jangid on 9/22/24.
+//
+
+import Testing
+@testable import Moonlight
+
+struct MoonlightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
