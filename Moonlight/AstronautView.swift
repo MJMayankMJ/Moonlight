009c5e0847fca5d30daf5244c3aaf3466971d7d2
MJMayankMJ <226301131@gkv.ac.in>
2024-10-09 22:23:19 +0530

    FormattedDate applied to MissionView

--- Moonlight/AstronautView.swift
+++ Moonlight/AstronautView.swift
@@ -5,4 +5,31 @@
 //  Created by Mayank Jangid on 10/9/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct AstronautView : View {
+    let astronaut: Astronaut
+    var body: some View {
+        ScrollView{
+            VStack{
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(.rect(cornerRadius: 10))
+                    .padding(.horizontal, 5)
+                
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+#Preview {
+    let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+    return AstronautView(astronaut: astronauts["aldrin"]!)
+        .preferredColorScheme(.dark)
+}
--- Moonlight/Color-Theme.swift
+++ Moonlight/Color-Theme.swift
@@ -5,4 +5,16 @@
 //  Created by Mayank Jangid on 10/5/24.
 //
 
-import Foundation
+import SwiftUI
+
+extension ShapeStyle where Self == Color {
+    static var darkBackground: Color {
+        Color(#colorLiteral(red: 0.224, green: 0.333, blue: 0.333, alpha: 1)) // #395555
+        //Color(#colorLiteral(red: 0.6, green: 0.722, blue: 0.78, alpha: 1)) // #99b8c7
+    }
+    static var lightBackground: Color {
+        //Color(#colorLiteral(red: 0.137, green: 0.835, blue: 0.835, alpha: 1)) // #23d5d5
+        //Color(#colorLiteral(red: 0.561, green: 0.851, blue: 0.984, alpha: 1)) // #8fd9fb
+        Color(#colorLiteral(red: 0.224, green: 0.667, blue: 0.667, alpha: 1)) // #39aaaa
+    }
+}
